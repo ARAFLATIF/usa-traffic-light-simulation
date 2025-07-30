b5ca44893bacbf6571fd83f078aeb0b0a60ef75d
Araf Ibne Latif <124219901+ARAFLATIF@users.noreply.github.com>
2025-07-30 10:08:41 -0500

    Update traffic_light.c

--- src/traffic_light.c
+++ src/traffic_light.c
@@ -4,37 +4,40 @@
 // Initialize a traffic light with default values
 void initializeTrafficLight(TrafficLight* light, int id) {
     light->id = id;                 // Assign a unique ID to the traffic light
-    light->state = RED;             // Set the initial state to RED
+    light->currentState = RED;             // Set the initial state to RED
     light->duration = DEFAULT_RED_DURATION; // Use the default RED duration from config.h
-    light->remaining_time = light->duration; // Set the remaining time to the initial state's duration
+    light->remainingTime = light->duration; // Set the remaining time to the initial state's duration
+    light->timeElapsed = 0;         // Initialize elapsed time
 }
 
 // Update the state of the traffic light based on its remaining time
 void updateTrafficLight(TrafficLight* light) {
     // Decrease the remaining time for the current state
-    light->remaining_time--;
+    light->remainingTime--;
+    light->timeElapsed++;
 
     // If the remaining time reaches 0, switch to the next state
-    if (light->remaining_time <= 0) {
-        switch (light->state) {
+    if (light->remainingTime <= 0) {
+        switch (light->currentState) {
             case RED: // If the current state is RED, switch to GREEN
-                light->state = GREEN;
+                light->currentState = GREEN;
                 light->duration = DEFAULT_GREEN_DURATION; // Set the duration for GREEN
                 break;
 
             case GREEN: // If the current state is GREEN, switch to YELLOW
-                light->state = YELLOW;
+                light->currentState = YELLOW;
                 light->duration = DEFAULT_YELLOW_DURATION; // Set the duration for YELLOW
                 break;
 
             case YELLOW: // If the current state is YELLOW, switch to RED
-                light->state = RED;
+                light->currentState = RED;
                 light->duration = DEFAULT_RED_DURATION; // Set the duration for RED
                 break;
         }
 
-        // Reset the remaining time for the new state
-        light->remaining_time = light->duration;
+        // Reset the remaining time and elapsed time for the new state
+        light->remainingTime = light->duration;
+        light->timeElapsed = 0;
     }
 }
 
@@ -55,5 +58,5 @@ const char* getLightStateName(LightState state) {
 // Debug function to display the traffic light's current status (optional)
 void printTrafficLightStatus(const TrafficLight* light) {
     printf("Traffic Light %d: State = %s, Remaining Time = %d seconds\n", 
-           light->id, getLightStateName(light->state), light->remaining_time);
+           light->id, getLightStateName(light->currentState), light->remainingTime);
 }
